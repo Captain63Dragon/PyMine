89ca710c2b76fe901add750b30ac3c54e74af240
Milo Weinberg <iapetus011@gmail.com>
2021-01-25 09:23:18 -0500

    fix logging not patching prompt back

--- pymine/util/logging.py
+++ pymine/util/logging.py
@@ -1,4 +1,5 @@
 from prompt_toolkit import print_formatted_text, ANSI
+from prompt_toolkit.patch_stdout import patch_stdout
 from prompt_toolkit import PromptSession
 import traceback
 import time
@@ -35,25 +36,35 @@ class Logger:
     def debug(self, *message):
         if self._debug:
             message = " ".join([str(m) for m in message])
-            print_formatted_text(ANSI(f"{WHITE}[{nice_time()} {GREY}DEBUG{WHITE}]: {GREY}{message}{END}"))
+
+            with patch_stdout():
+                print_formatted_text(ANSI(f"{WHITE}[{nice_time()} {GREY}DEBUG{WHITE}]: {GREY}{message}{END}"))
 
     def info(self, *message):
         message = " ".join([str(m) for m in message])
-        print_formatted_text(ANSI(f"{BRIGHT}{WHITE}[{nice_time()} {BLUE}INFO{WHITE}]: {message}{END}"))
+
+        with patch_stdout():
+            print_formatted_text(ANSI(f"{BRIGHT}{WHITE}[{nice_time()} {BLUE}INFO{WHITE}]: {message}{END}"))
 
     def warn(self, *message):
         message = " ".join([str(m) for m in message])
-        print_formatted_text(ANSI(f"{BRIGHT}{WHITE}[{nice_time()} {YELLOW}WARNING{WHITE}]: {YELLOW}{message}{END}"))
+
+        with patch_stdout():
+            print_formatted_text(ANSI(f"{BRIGHT}{WHITE}[{nice_time()} {YELLOW}WARNING{WHITE}]: {YELLOW}{message}{END}"))
 
     warning = warn
 
     def error(self, *message):
         message = " ".join([str(m) for m in message])
-        print_formatted_text(ANSI(f"{BRIGHT}{WHITE}[{nice_time()} {RED}ERROR{WHITE}]: {RED}{message}{END}"))
+
+        with patch_stdout():
+            print_formatted_text(ANSI(f"{BRIGHT}{WHITE}[{nice_time()} {RED}ERROR{WHITE}]: {RED}{message}{END}"))
 
     def critical(self, *message):
         message = " ".join([str(m) for m in message])
-        print_formatted_text(ANSI(f"{BRIGHT}{WHITE}{BG_RED}[{nice_time()} CRITICAL]: {message}{END}"))
+
+        with patch_stdout():
+            print_formatted_text(ANSI(f"{BRIGHT}{WHITE}{BG_RED}[{nice_time()} CRITICAL]: {message}{END}"))
 
     @staticmethod
     def f_traceback(e: BaseException):
